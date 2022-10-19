05959abddf036c0b0adfd33929f08228b121914a
abdraevaeleonora <abdraevaeleonora@gmail.com>
2022-10-19 15:09:44 +0600

    com

--- lib/components/continent_card.dart
+++ lib/components/continent_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+//import 'package:flutter_svg/svg.dart';
 import 'package:game_test/models/continent.dart';
 
 class ContinentCard extends StatelessWidget {
